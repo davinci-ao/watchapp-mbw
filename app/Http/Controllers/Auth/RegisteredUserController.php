7b6e4303a63c773568cf21846e92ef8fa7d54729
Peter Snoek <petersnoek@davinci.nl>
2021-11-25 10:19:44 +0100

    prevent logging in automatically after a register, to prevent spammers

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -47,7 +47,8 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
+        // disable logging in automatically after register, to prevent spammers
+        // Auth::login($user);
 
         return redirect(RouteServiceProvider::HOME);
     }
